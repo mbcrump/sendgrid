a34c3dd5f2a6cfb18adcc7704b0b1322d6d195b3
Michael Crump <mbcrump29@gmail.com>
2017-12-30 11:16:17 -0800

    initial push

---
+++ SendEmail/Program.cs
@@ -0,0 +1,41 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute().Wait();
+
+        }
+        static async Task Execute()
+        {
+            var client = new SendGridClient("ADD-SENDGRID-API-KEY-HERE");
+            var msg = new SendGridMessage();
+
+            msg.SetFrom(new EmailAddress("michael@michaelcrump.net", "Azure Tips and Tricks"));
+
+            var recipients = new List<EmailAddress>
+                {
+                    new EmailAddress("test@gmail.com"),
+                    new EmailAddress("test@michaelcrump.net"),
+                    new EmailAddress("test@michaelcrump.net")
+                };
+            msg.AddTos(recipients);
+
+            msg.SetSubject("Testing the SendGrid C# Library");
+
+            msg.AddContent(MimeType.Text, "Hello World plain text!");
+            msg.AddContent(MimeType.Html, "<p>Hello World!</p>");
+            var response = await client.SendEmailAsync(msg);
+
+        }
+    }
+}
